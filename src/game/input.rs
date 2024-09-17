d75c8b187c5720b91ba1f98fb12e415656c4afed
Nixon <voxell.technologies@gmail.com>
2024-09-17 11:27:08 +0800

    feat: leafwing input system
    Co-Authored-By: Lew Xiu Zhen <123133455+callmewho@users.noreply.github.com>

--- src/game/input.rs
+++ src/game/input.rs
@@ -5,38 +5,49 @@ pub struct InputPlugin;
 
 impl Plugin for InputPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(InputManagerPlugin::<PlayerAction>::default());
+        app.add_plugins(InputManagerPlugin::<PlayerAction>::default())
+            .init_resource::<ActionState<PlayerAction>>()
+            .insert_resource(PlayerAction::input_map())
+            .add_systems(Update, handle_player_input);
     }
 }
 
-#[derive(PartialEq, Eq, Clone, Copy, Hash, Debug, Reflect)]
-enum PlayerAction {
-    Run,
+// Handle player input actions
+fn handle_player_input(action_state: Res<ActionState<PlayerAction>>) {
+    if action_state.pressed(&PlayerAction::Move) {
+        if let Some(axis_pair) = action_state.clamped_axis_pair(&PlayerAction::Move) {
+            println!("Move: ({}, {})", axis_pair.x(), axis_pair.y());
+        }
+    }
+
+    if action_state.pressed(&PlayerAction::Jump) {
+        println!("jump");
+    }
+
+    if action_state.pressed(&PlayerAction::UseItem) {
+        println!("use item");
+    }
+}
+
+#[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug, Reflect)]
+pub enum PlayerAction {
+    Move,
     Jump,
     UseItem,
 }
 
-impl Actionlike for PlayerAction {
-    fn input_control_kind(&self) -> InputControlKind {
-        match self {
-            PlayerAction::Run => InputControlKind::DualAxis,
-            _ => InputControlKind::Button,
-        }
-    }
-}
-
 impl PlayerAction {
     /// Define the default bindings to the input
-    pub(crate) fn default_input_map() -> InputMap<Self> {
+    pub(crate) fn input_map() -> InputMap<Self> {
         let mut input_map = InputMap::default();
 
         // Default gamepad input bindings
-        input_map.insert_dual_axis(Self::Run, GamepadStick::LEFT);
+        input_map.insert(Self::Move, DualAxis::left_stick());
         input_map.insert(Self::Jump, GamepadButtonType::South);
         input_map.insert(Self::UseItem, GamepadButtonType::RightTrigger2);
 
         // Default kbm input bindings
-        input_map.insert_dual_axis(Self::Run, KeyboardVirtualDPad::WASD);
+        input_map.insert(Self::Move, VirtualDPad::wasd());
         input_map.insert(Self::Jump, KeyCode::Space);
         input_map.insert(Self::UseItem, MouseButton::Left);
 
