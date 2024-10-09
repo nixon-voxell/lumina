db6b0f97a90cfdf603614fb780449e5ae5c786fd
nixon-voxell <voxell.technologies@gmail.com>
2024-10-09 10:03:19 +0800

    Add convert_effector system

--- src/shared/effector.rs
+++ src/shared/effector.rs
@@ -1,18 +1,51 @@
+use avian2d::prelude::*;
 use bevy::prelude::*;
 
 pub struct EffectorPlugin;
 
 impl Plugin for EffectorPlugin {
     fn build(&self, app: &mut App) {
+        app.add_systems(Update, convert_effector);
+
         app.register_type::<EffectorPopupMsg>()
-            .register_type::<InteractableEffector>();
+            .register_type::<InteractableEffector>()
+            .register_type::<Effector>();
     }
 }
 
+/// Convert all effectors to colliders and sensors.
+fn convert_effector(mut commands: Commands, q_effectors: Query<(&Effector, Entity)>) {
+    for (effector, entity) in q_effectors.iter() {
+        commands.entity(entity).insert((
+            Collider::try_from_constructor(effector.collider.clone()).unwrap(),
+            Sensor,
+            effector.rigidbody,
+        ));
+
+        commands.entity(entity).remove::<Effector>();
+    }
+}
+
+/// Popup message when player enters the effector collision range.
 #[derive(Component, Reflect, Default, Debug)]
 #[reflect(Component)]
 pub struct EffectorPopupMsg(pub String);
 
-#[derive(Component, Reflect, Default, Debug)]
+/// Popup the interactable button when player enters the effector collision range.
+///
+/// This also acts as a marker that a particular [`Sensor`] is interactable.
+#[derive(Component, Reflect, Debug)]
 #[reflect(Component)]
 pub struct InteractableEffector;
+
+/// A constructor for effector which will be converted into avian sensor related components:
+///
+/// - [`Rigidbody`]
+/// - [`Collider`]
+/// - [`Sensor`]
+#[derive(Component, Reflect, Debug)]
+#[reflect(Component)]
+pub struct Effector {
+    pub rigidbody: RigidBody,
+    pub collider: ColliderConstructor,
+}
