40e31c14e219e7bbb2054da0acc0184836a304ca
Nixon <43715558+nixon-voxell@users.noreply.github.com>
2025-04-11 00:43:30 +0800

    Objective area rotation (#173)
    * Add area based objective logic, update rand crate
    
    * Minor clippy fix for release mode
    
    * Optimize out name

--- crates/lumina_shared/src/player/ammo.rs
+++ crates/lumina_shared/src/player/ammo.rs
@@ -82,12 +82,9 @@ fn fire_ammo(
     let ammo_pool = &mut ammo_pools[*ammo_type as usize];
 
     let ammo_entity = ammo_pool.get_unused_or_spawn(|| {
-        let mut cmd = commands.spawn(InitAmmoBundle::new(*ammo_type, collider.clone()));
-
-        #[cfg(debug_assertions)]
-        cmd.insert(Name::new(ammo_type.as_ref().to_string()));
-
-        cmd.id()
+        commands
+            .spawn(InitAmmoBundle::new(*ammo_type, collider.clone()))
+            .id()
     });
 
     // Initialize fire ammo components.
