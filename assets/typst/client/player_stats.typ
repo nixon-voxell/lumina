bca01e2316113c5f1e3446e50c6d044df616a0bb
Nixon <voxell.technologies@gmail.com>
2025-04-14 12:29:02 +0800

    Add health and shadow effect transparency

--- assets/typst/client/player_stats.typ
+++ assets/typst/client/player_stats.typ
@@ -8,12 +8,54 @@
 
   box(width: 100%, height: 100%)[
     #for player in players {
+      let fill = if player.is_local_team { blue } else { red }
+      if player.is_local_team == false {
+        fill = fill.transparentize(player.transparency * 100%)
+      }
+
+      place(
+        dx: player.x * 1pt,
+        dy: player.y * 1pt,
+      )[
+        #place(center + horizon)[
+          #rect(
+            stroke: (
+              paint: fill.transparentize(80%),
+              thickness: 0.4em,
+              dash: (array: (2em, 3em), phase: 1em),
+            ),
+            width: 5em,
+            height: 5em,
+          )
+        ]
+      ]
+
       place(
         dx: player.x * 1pt,
         dy: player.y * 1pt + 4em / scale,
       )[
         #place(center)[
-          #text(fill: if player.is_local { blue } else { red }, player.name)
+          #text(
+            fill: fill,
+            player.name,
+          )\
+          #box(
+            fill: fill.darken(40%).transparentize(60%),
+            stroke: 0.15em + fill.darken(60%).transparentize(60%),
+            width: 4em,
+            height: 1em,
+            radius: 0.3em,
+            clip: true,
+          )[
+            #place(
+              box(
+                fill: fill.lighten(20%),
+                width: 100% * player.health,
+                height: 100%,
+                radius: 0.3em,
+              ),
+            )
+          ]
         ]
       ]
     }
--- crates/lumina_client/src/ui/game_ui/player_stats.rs
+++ crates/lumina_client/src/ui/game_ui/player_stats.rs
@@ -22,7 +22,16 @@ impl Plugin for PlayerStatsPlugin {
 
 fn player_stats(
     q_spaceships: Query<
-        (&TeamType, &PlayerId, &GlobalTransform),
+        (
+            &TeamType,
+            &Health,
+            &MaxHealth,
+            Option<&AbilityEffect>,
+            Option<&AbilityCooldown>,
+            Has<ShadowAbilityConfig>,
+            &GlobalTransform,
+            &PlayerId,
+        ),
         (With<Spaceship>, With<SourceEntity>),
     >,
     q_game_camera: Query<(&GlobalTransform, &OrthographicProjection, &Camera), With<GameCamera>>,
@@ -44,18 +53,39 @@ fn player_stats(
     };
     func.scale = proj.scale as f64;
 
-    for (team_type, id, transform) in q_spaceships.iter() {
+    for (
+        team_type,
+        health,
+        max_health,
+        ability_effect,
+        ability_cooldown,
+        has_shadow_ability,
+        transform,
+        id,
+    ) in q_spaceships.iter()
+    {
         let name = NAMES[id.to_bits() as usize % NAMES.len()];
+        let transparency = match (ability_effect, ability_cooldown) {
+            (Some(effect), None) if has_shadow_ability => effect.elapsed_secs() / 0.3,
+            (None, Some(cooldown)) if has_shadow_ability => 1.0 - (cooldown.elapsed_secs() / 0.5),
+            _ => 0.0,
+        } as f64;
+
         let translation = camera
             .world_to_viewport(camera_transform, transform.translation())
             .unwrap_or_default();
+
         func.players.push(dict! {
-            "is_local" => *team_type == local_team_type,
+            "is_local_team" => *team_type == local_team_type,
             "x" => translation.x as f64,
             "y" => translation.y as f64,
             "name" => name,
+            "health" => (**health / **max_health) as f64,
+            "transparency" => transparency
         });
     }
+
+    func.dummy_update = func.dummy_update.wrapping_add(1);
 }
 
 #[derive(TypstFunc, Resource, Default)]
